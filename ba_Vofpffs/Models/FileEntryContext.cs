a83d243191e7e54ca87c1a8a13cb44a11103bbf1
34324234324 <starkids12@hotmail.de>
2018-04-23 16:43:37 +0200

    init

---
+++ ba_Vofpffs/Models/FileEntryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ba_Vofpffs.Models
+{
+    public class FileEntryContext : DbContext
+    {
+        public FileEntryContext(DbContextOptions<FileEntryContext> options) : base (options) { }
+
+        public DbSet<FileEntryItem> FileEntryItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FileEntryItem> ().ToTable ("FileEntry");
+        }
+    }
+}
